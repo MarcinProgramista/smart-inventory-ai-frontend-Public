a4cc16c9dd57af355a8ced4230b316b584405fb4
Marcin Czapla <marcin.czapla.programista@gmail.com>
2026-01-04 18:25:25 +0100

    feat(suppliers): add empty state to suppliers list

--- src/components/suppliers/SuppliersList.jsx
+++ src/components/suppliers/SuppliersList.jsx
@@ -1,9 +1,20 @@
+import styled from "styled-components";
 import ListHeader from "../shared/header/ListHeader";
-
-export default function SuppliersList({ onAdd }) {
+const EmptyState = styled.div`
+  padding: 3rem;
+  text-align: center;
+  opacity: 0.7;
+`;
+export default function SuppliersList({ onAdd, suppliers = [] }) {
+  const isEmpty = suppliers.length === 0;
   return (
     <>
       <ListHeader heading="Suppliers" onAdd={onAdd} addTitle="Add Supplier" />
+      {isEmpty && (
+        <EmptyState>
+          <p>You don't have any suppliers yet.</p>
+        </EmptyState>
+      )}
     </>
   );
 }
