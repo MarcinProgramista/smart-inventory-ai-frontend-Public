9dfc423b7947a74d20cabfe97d85f95df33407c1
Marcin Czapla <marcin.czapla.programista@gmail.com>
2026-01-04 18:17:39 +0100

    feat(suppliers): add suppliers list header

--- src/components/suppliers/SuppliersList.jsx
+++ src/components/suppliers/SuppliersList.jsx
@@ -1,3 +1,9 @@
-export default function SuppliersList() {
-  return <div>Suppliers list works</div>;
+import ListHeader from "../shared/header/ListHeader";
+
+export default function SuppliersList({ onAdd }) {
+  return (
+    <>
+      <ListHeader heading="Suppliers" onAdd={onAdd} addTitle="Add Supplier" />
+    </>
+  );
 }
