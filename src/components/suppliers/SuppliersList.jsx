875be3d647ed7ade6d9701868d8fc1f49af5ccc9
Marcin Czapla <marcin.czapla.programista@gmail.com>
2026-01-04 19:09:44 +0100

    feat(suppliers): render suppliers table (no pagination)

--- src/components/suppliers/SuppliersList.jsx
+++ src/components/suppliers/SuppliersList.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import ListHeader from "../shared/header/ListHeader";
+import { Table, TableWrapper, Td, Th, Tr } from "../shared/table/Table.styles";
 const EmptyState = styled.div`
   padding: 3rem;
   text-align: center;
@@ -10,10 +11,37 @@ export default function SuppliersList({ onAdd, suppliers = [] }) {
   return (
     <>
       <ListHeader heading="Suppliers" onAdd={onAdd} addTitle="Add Supplier" />
-      {isEmpty && (
+      {isEmpty ? (
         <EmptyState>
           <p>You don't have any suppliers yet.</p>
         </EmptyState>
+      ) : (
+        <TableWrapper>
+          <Table>
+            <thead>
+              <Tr>
+                <Th>Name</Th>
+                <Th>City</Th>
+                <Th>Country</Th>
+                <Th>Contact</Th>
+              </Tr>
+            </thead>
+            <tbody>
+              {suppliers.map((s) => (
+                <Tr key={s.id}>
+                  <Td>{s.name}</Td>
+                  <Td>{s.city}</Td>
+                  <Td>{s.country}</Td>
+                  <Td>
+                    {s.contact
+                      ? `${s.contact.first_name} ${s.contact.last_name}`
+                      : "-"}
+                  </Td>
+                </Tr>
+              ))}
+            </tbody>
+          </Table>
+        </TableWrapper>
       )}
     </>
   );
